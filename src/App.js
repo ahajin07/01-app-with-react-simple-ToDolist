c1b3da6fe1f72da36f48368aa1236f043e286c0e
rahul jha <irahul.jha.007@gmail.com>
2025-03-21 20:11:40 +0530

    first react project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react';
+import Header from './components/Header.jsx';
+import TodoItem from './components/TodoItem.jsx';
+import Buttomn from './components/Button.jsx';
+import './styles.css';
+const App=()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo-container">
+      <Header />
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />      
+      <Buttomn />
     </div>
   );
-}
-
+};
 export default App;
