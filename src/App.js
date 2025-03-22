c0b09f6b2050bfb1a83896600a41b9fe9f5df271
rahul jha <irahul.jha.007@gmail.com>
2025-03-22 15:50:59 +0530

    final changes

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import Header from './components/Header.jsx';
 import TodoItem from './components/TodoItem.jsx';
 import Buttomn from './components/Button.jsx';
 import './styles.css';
+import CounterComponent from './components/CounterComponent.jsx';
+
 const App=()=> {
   return (
     <div className="todo-container">
+      <CounterComponent />
       <Header />
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />      
+      <TodoItem text = "eat"/>
+      <TodoItem text = "code"/>
+      <TodoItem text = "play" />
+      <TodoItem text =" washroom"/>
+      <TodoItem text= "sleep"/>      
       <Buttomn />
     </div>
   );
